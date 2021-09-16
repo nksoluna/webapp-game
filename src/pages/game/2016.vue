cc074193d77b57cf5639df762e0e3b8a57e82ac9
THANAT HIRUNYATHON <6110110184@psu.ac.th>
2021-09-16 14:49:19 +0700

    add image

--- src/pages/game/2016.vue
+++ src/pages/game/2016.vue
@@ -1,6 +1,7 @@
 <template>
 
     <p> {{products[index].name}}</p>
+    <img src="~assets/2016-game/1.jpg" id="game-img">
     <p> {{products[index].date}}</p>
     <p>price: {{products[index].price}}$ </p>
     <p>{{products[index].description}}</p>
@@ -19,53 +20,57 @@ export default{
     data() {
         return {
             index:0,
-            url:"~assets/quasar-logo-vertical.svg",
              products: [
       {
         name: "Rule of Survival",
         price: 50,
+        image:"https://static.thairath.co.th/media/dFQROr7oWzulq5Fa4VZ4C5y9f8QC0Wa77NJlPB6O2Fniz7zz4ErXEG8Bc9ycEzdubUo.jpg",
         description: "This is survival game, developed and published by NetEase Games",
         date: "November 2017"
       },
       {
         name: "PUBG",
         price: 400,
+        image:"https://play-lh.googleusercontent.com/JRd05pyBH41qjgsJuWduRJpDeZG0Hnb0yjf2nWqO7VaGKL10-G5UIygxED-WNOc3pg",
         description: "PUBG is an online multiplayer battle royale game developed and published by PUBG Corporation.",
         date: "August 2018"
       },
       {
         name: "CSGO",
         price: 300,
+        image:"https://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1629301045",
         description: "CSGO is a multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series.",
         date: "August 2012"
       },
       {
         name: "Valorant",
         price: 0,
+        image:"https://s.isanook.com/ga/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2dhLzAvdWQvMjEyLzEwNjM1MjEvdmFsb3JhbnQwMy5qcGc=.jpg",
         description: "Valorant is a free-to-play first-person hero shooter developed and published by Riot Games, for Microsoft Windows.",
         date: "June 2020"
       },
       {
         name: "Dota 2",
         price: 0,
+        image:"https://cdn.akamai.steamstatic.com/steam/apps/570/header.jpg?t=1630016563",
         description: "Dota 2 is a free-to-play multiplayer online battle arena (MOBA) video game developed and published by Valve.",
         date: "July 2013"
       },
-      {
-        name: "The Last of Us Part II",
-        price: 600,
-        description: "The Last of Us Part II is a 2020 action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment for the PlayStation 4.",
-        date: "May 2020"
-      }
     ]
     
         }
     },methods: {
         previous:function(){
             if(this.index>=1) this.index -= 1;
+            const url = this.products[this.index].image;
+            const image = document.getElementById("game-img");
+            image.src = url;
         },
         next:function(){
             if(this.index<this.products.length) this.index += 1;
+            const url = this.products[this.index].image;
+            const image = document.getElementById("game-img");
+            image.src = url;
         }
     },
     setup() {
---
+++ src/pages/game/2020.vue
@@ -0,0 +1,53 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default{
+  data() {
+    return {
+      products: [
+      {
+        name: "PUBG",
+        image: "https://upload.wikimedia.org/wikipedia/en/9/9f/Pubgbattlegrounds.png",
+        price: 400,
+        description: "PlayerUnknown's Battlegrounds (PUBG, also known as PUBG: Battlegrounds) is an online multiplayer battle royale game developed and published by PUBG Corporation, a subsidiary of South Korean video game company Bluehole. The game is based on previous mods that were created by Brendan PlayerUnknown Greene for other games, inspired by the 2000 Japanese film Battle Royale, and expanded into a standalone game under Greene's creative direction. In the game, up to one hundred players parachute onto an island and scavenge for weapons and equipment to kill others while avoiding getting killed themselves. The available safe area of the game's map decreases in size over time, directing surviving players into tighter areas to force encounters. The last player or team standing wins the round.",
+        date: "August 2018"
+      },
+      {
+        name: "CSGO",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/CSGOcoverMarch2020.jpg/220px-CSGOcoverMarch2020.jpg",
+        price: 300,
+        description: "Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series. Developed for over two years, Global Offensive was released for Windows, macOS, Xbox 360, and PlayStation 3 in August 2012, and for Linux in 2014. Valve still regularly updates the game, both with smaller balancing patches and larger content additions.",
+        date: "August 2012"
+      },
+      {
+        name: "Valorant",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/1024px-Valorant_logo_-_pink_color_version.svg.png",
+        price: 0,
+        description: "Valorant (stylized as VALORANT) is a free-to-play first-person hero shooter developed and published by Riot Games, for Microsoft Windows. First teased under the codename Project A in October 2019, the game began a closed beta period with limited access on April 7, 2020, followed by an official release on June 2, 2020. The development of the game started in 2014.",
+        date: "June 2020"
+      },
+      {
+        name: "Dota 2",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/0b/Dota_2_%28Steam_2019%29.jpg",
+        price: 0,
+        description: "Dota 2 is a multiplayer online battle arena (MOBA) video game developed and published by Valve. The game is a sequel to Defense of the Ancients (DotA), which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos. Dota 2 is played in matches between two teams of five players, with each team occupying and defending their own separate base on the map. Each of the ten players independently controls a powerful character, known as a hero, who all have unique abilities and differing styles of play. During a match players collect experience points and items for their heroes to successfully defeat the opposing team's heroes in player versus player combat. A team wins by being the first to destroy the other team's Ancient, a large structure located within their base.",
+        date: "July 2013"
+      },
+      {
+        name: "The Last of Us Part II",
+        image: "https://upload.wikimedia.org/wikipedia/en/4/4f/TLOU_P2_Box_Art_2.png",
+        price: 600,
+        description: "The Last of Us Part II is a 2020 action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment for the PlayStation 4. Set five years after The Last of Us (2013), the game focuses on two playable characters in a post-apocalyptic United States whose lives intertwine: Ellie, who sets out for revenge after suffering a tragedy, and Abby, a soldier who becomes involved in a conflict between her militia and a religious cult. The game is played from the third-person perspective and allows the player to fight human enemies and cannibalistic zombie-like creatures with firearms, improvised weapons, and stealth.",
+        date: "May 2020"
+      }
+    ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
